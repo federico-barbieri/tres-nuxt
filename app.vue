bc2f4533357cb9add2831e895809adc78462f033
Federico Barbieri <federicobarbieri54@gmail.com>
2023-10-08 00:55:55 +0200

    cool stuff

---
+++ app.vue
@@ -0,0 +1,73 @@
+<template>
+  
+  <main>
+    <Canvas :color="colorChanger" />
+
+    <form class="panel-form" action="">
+      <h1>Welcome to {{ schoolName }}</h1>
+      <label for="name">
+        Name:
+        <input type="text" name="name" id="name" v-model="schoolName">
+
+
+      </label>
+
+      <label for="color">
+        Color:
+        <input type="text" name="color" id="color" v-model="inputValue">
+
+      </label>
+    </form>
+
+    
+
+
+  </main>
+
+
+</template>
+
+<script setup>
+
+const schoolName = ref('');
+
+
+const inputValue = ref('');
+const colorChanger = ref('');
+
+// Watch for changes in inputValue and update colorChanger accordingly
+watch(inputValue, (newValue) => {
+  colorChanger.value = newValue;
+});
+
+
+</script>
+
+<style scoped>
+
+main{
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: flex-start;
+
+}
+
+h1{
+  color: black;
+}
+
+.panel-form{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  border: 2px solid red;
+  width: 20%;
+  height: 60%;
+  margin: 0 auto 0 5rem;
+}
+
+</style>
---
+++ components/Canvas.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="wrapper">
+  
+   <TresCanvas width="100%" height="100%" :clear-color="color" preset="realistic">
+        <OrbitControls />
+        <TresMesh :scale="[2, 0.5, 1]">
+            <TresBoxGeometry />
+            <TresMeshNormalMaterial />
+        </TresMesh>
+  </TresCanvas>
+  </div>
+  
+  
+  
+  </template>
+  
+  <script setup>
+
+const props = defineProps({
+  color: {
+    type: String, // Replace PropType with the type of your prop (e.g., String, Number, Object, etc.)
+    required: true, // Set to true if the prop is required
+  },
+});  
+  
+  </script>
+  
+  <style scoped>
+  
+  .wrapper{
+    width: 40vw;
+    height: 60vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 0 0 5rem;
+  }
+  
+  </style>
